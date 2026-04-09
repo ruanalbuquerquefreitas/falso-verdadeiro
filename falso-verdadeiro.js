4b050f9725190582bc299a807edd98f69e2943ea
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-04-09 14:54:27 +0000

    sarvo

--- falso-verdadeiro.js
+++ falso-verdadeiro.js
@@ -1,5 +1,21 @@
 //booleano
 
-const usuarioLogado = true;
-const contapaga =false;
+//const usuarioLogado = true;
+//const contapaga =false;
     
+// falso e verdadeiro
+
+//console.log(0==false);
+//console.log(""==false);
+//console.log(1==true);
+
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let texto = "Alura";
+
+console.log (typeof numero);
+console.log(typeof texto);
