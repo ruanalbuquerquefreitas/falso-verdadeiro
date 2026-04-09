4312dcafcdf3f75be6dbc3d4062ebc9ee7e3c1da
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-04-09 15:19:58 +0000

    sarvo

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dado
+// booleano
+
+// conversão implícita 
+const numero = 456;
+const numeroString = Number ("456");
+
+//Number()
+//String()
+
+console.log (numero + numeroString);
+
+//conversão explícita
