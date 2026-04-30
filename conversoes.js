afa437dce90f3cd28a8d2b1ed172b58ddfd4d028
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-04-30 15:21:58 +0000

    ver

--- conversoes.js
+++ conversoes.js
@@ -2,12 +2,21 @@
 // booleano
 
 // conversão implícita 
-const numero = 456;
-const numeroString = Number ("456");
+//const numero = 456;
+//const numeroString = Number ("456");
 
 //Number()
 //String()
 
-console.log (numero + numeroString);
+//console.log (numero + numeroString);
 
 //conversão explícita
+
+//calculadora inteligente 
+
+const preco = "100.50";
+const desconto = "10";
+
+console.log (preco - desconto);
+console.log (preco * 2);
+console.log (preco / 2)
