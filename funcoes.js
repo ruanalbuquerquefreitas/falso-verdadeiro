05dcfee783e20a1d68a13460b5746cdd80106d95
ruanalbuquerquefreitas <ruan.albuquerque.freitas@escola.pr.gov.br>
2026-05-21 15:13:06 +0000

    sarvado

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+const num1 = 3
+const num2 = 4
+function multiplicar (num1, num2){
+    return num1 * num2;
+}
+console.log(multiplicar(num1, num2))
+console.log(multiplicar(4,5))
---
+++ template-strings.js
@@ -0,0 +1,7 @@
+const nome = "Ruan";
+const idade = 2026-2010;
+const cidadeDeNascimento = "Araruna";
+//const (apresentacao) = "Meu nome é " + nome + ",minha idade é " + idade + " e nasci na cidade " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+console.log(apresentacao);
